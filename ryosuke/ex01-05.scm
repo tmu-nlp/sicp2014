765b0969aaf7cb0d868116f9ad10752216803e33
ryosuke <tmcit.miyazaki@gmail.com>
2014-10-23 02:54:37 +0900

    delete once in order to subtree

--- ryosuke/ex01-05.scm
+++
@@ -1,8 +0,0 @@
-;;with an applicative-order evaluation, this evaluation never finish.
-;;wit an normal-order evaluation, interpreter output '0'.
-
-(define (p) (p))
-(define (test x y)
-  (if (= x 0) 0 y))
-
-(print (test 0 (p)))
