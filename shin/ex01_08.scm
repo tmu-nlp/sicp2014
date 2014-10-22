ccffde8585fde711728ccc016d79c09076f4bb76
shin-kanouchi <tenzyonerai@gmail.com>
2014-10-22 18:29:05 +0900

    shin!

---
+++ shin/ex01_08.scm
@@ -0,0 +1,22 @@
+#!/user/bin/gosh
+
+(define (cbrt-iter guess pre-guess x)
+    (if (good-enough? guess pre-guess)
+        guess
+        (cbrt-iter (improve guess x) guess x)))
+
+(define (improve guess x)
+    (/ (+ (/ x (* guess guess)) 
+          (* 2 guess))
+       3))
+
+(define (good-enough? guess pre-guess)
+    (< (/ (abs (- guess pre-guess)) guess) 0.0001))
+
+(define (cbrt x)
+    (cbrt-iter 1.0 100.0 x))
+
+(print (cbrt 8))
+(print (cbrt 27))
+(print (cbrt 0.000002))
+(print (cbrt 200000000000000000000))
