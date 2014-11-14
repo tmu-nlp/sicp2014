6213acc8867dce2e10f39e7f1eff8c0607065409
堺澤勇也 <ksf.doingmorewithless@gmail.com>
2014-11-14 15:24:42 +0900

    modified

--- sakaizawa/ex01-17.scm
+++ sakaizawa/ex01-17.scm
@@ -1,20 +1,21 @@
-#!/usr/bin/gosh
-;; -*- coding:utf-8 -*-
+#usr/bin/gosh
+ -*- coding:utf-8 -*-
 ;;実行方法　gosh ex01-17.scm
 
-(define (double x) (* x 2))
+(define (double x) (* x 2)) 
 
-(define (halve x) (/ x 2))
+(define (halve x) (/ x 2)) 
 
-(define (even? n) (= (remainder n 2) 0))
+(define (even? n) (= (remainder n 2) 0)) 
 
 (define (multiply x y)
-  (cond ((= x 1) y)
+  (cond ((= x 0) 0)
+        ((= y 0) 0)
+        ((= x 1) y)
         ((even? x) (multiply (halve x) (double y)))
         (else (+ y (multiply (- x 1) y)))))
 
 (define (mul a b) (multiply a b))
-
 
 (print "(mul 3 4)")
 (print (mul 3 4))
--- sakaizawa/ex01-20.scm
+++ sakaizawa/ex01-20.scm
@@ -4,23 +4,76 @@
 
 (define (gcd a b)
   (if (= b 0)
-      a
-      (gcd b (remainder a b))))
+    a   
+    (gcd b (remainder a b))))
+
+; norma-order eval
+; 18
+(gcd 206 40) 
+
+(if (= 40 0) ; #f
+      206 
+      (gcd 40 (rem 206 40)))
+
+(if (= (rem 206 40) 0) ; #f
+      40  
+      (gcd (rem 206 40) (rem 40 (rem 206 40))))
+
+(if (= (rem 40 (rem 206 40)) 0) ; #f
+      (rem 206 40) 
+      (gcd (rem 40 (rem 206 40)) (rem (rem 206 40) (rem 40 (rem 206 40)))))
+
+(if (= (rem (rem 206 40) (rem 40 (rem 206 40))) 0) ; #f
+      (rem 40 (rem 206 40))
+      (gcd (rem (rem 206 40) (rem 40 (rem 206 40))) (rem (rem 40 (rem 206 40)) (rem (rem 206 40) (rem 40 (rem 206 40))))))
+
+(if (= (rem (rem 40 (rem 206 40)) (rem (rem 206 40) (rem 40 (rem 206 40)))) 0) ; #t
+      (rem (rem 206 40) (rem 40 (rem 206 40)))
+      (gcd (rem (rem 40 (rem 206 40)) (rem (rem 206 40) (rem 40 (rem 206 40)))) (rem (rem (rem 40 (rem 206 40))) (rem (rem 40 (rem 206 40)) (rem (rem 206 40) (rem 40 (rem 206 40)))))))
+
+(rem (rem 206 40) (rem 40 (rem 206 40)))
+(rem 6 (rem 40 6)) 
+(rem 6 4)
+2
 
 
-(gcd 206 40)
-  (gcd 40 6)
-  (gcd 6 4)
-  (gcd 4 2)
-  (gcd 2 0)
-;;2
+; applicative-order eval
+; 4 times
+(gcd 206 40) 
+(if (= 40 0) ; #f
+      206 
+      (gcd 40 (rem 206 40)))
 
-;;remainder演算は4回実行される
-;;作用的順序の場合、remainder演算は10回？
+(if (= 40 0) ; #f
+      206 
+      (gcd 40 6)) 
 
+(if (= 6 0) ; #f
+      40  
+      (gcd 6 (rem 40 6)))
 
+(if (= 6 0) ; #f
+      40  
+      (gcd 6 4)) 
 
-(define (gcd a b)
-  (cond ((= b 0) a)
-        (else (gcd b (remainder a b)))))
+(if (= 4 0) ; #f
+      6   
+      (gcd 4 (rem 6 4)))
 
+(if (= 4 0) ; #f
+      6   
+      (gcd 4 2)) 
+
+(if (= 2 0) ; #f
+      4   
+      (gcd 2 (rem 4 2)))
+
+(if (= 2 0) ; #f
+      4   
+      (gcd 2 0)) 
+
+(if (= 0 0) ; #t
+      2   
+      (gcd 2 0)) 
+
+2
