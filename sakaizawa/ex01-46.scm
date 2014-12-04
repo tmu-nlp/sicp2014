3542c06378c8d2da21a848bd31746bf9ec8a8c49
堺澤勇也 <ksf.doingmorewithless@gmail.com>
2014-12-04 18:37:57 +0900

    first commit

---
+++ sakaizawa/ex01-46.scm
@@ -0,0 +1,40 @@
+#!/usr/bin/gosh
+;; -*- coding:utf-8 -*-
+;;実行方法　gosh ex01-45.scm
+
+(define tolerance 0.00001)
+
+(define (square x) (* x x))
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+
+
+(define (iterative-improve enough? imp)
+  (lambda (guess)
+    (define (iter x)
+      (if (enough? x) x (iter (imp x))))
+    (iter guess)))
+
+(define (sqrt-itr x)
+  (define (good-enough? guess)
+    (< (abs (- x (square guess))) tolerance))
+  (define (improve guess) (average guess (/ x guess)))
+  ((iterative-improve good-enough? improve) 1.0))
+
+(define (fixed-point-itr f first-guess)
+  (define (close-enough? guess)
+    (< (abs (- guess (next guess))) tolerance))
+  (define (next guess) (f guess))
+  ((iterative-improve close-enough? next) first-guess))
+
+(print "(sqrt-itr 4)")
+(print (sqrt-itr 4))
+
+(print "(fixed-point-itr cos 1.0)")
+(print (fixed-point-itr cos 1.0))
+
+
+
