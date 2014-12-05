910270c29911eb5f59e35c08ef7c8c496f0b9e45
ryosuke <tmcit.miyazaki@gmail.com>
2014-12-05 14:58:09 +0900

    minor modified

--- ryosuke/ex01-29.scm
+++ ryosuke/ex01-29.scm
@@ -7,15 +7,15 @@
 
   (define h (/ (- b a) n))
 
-  (define (yk k a h)
+  (define (yk k a)
     (f (+ a (* k h))))
   
   (define (iter i temp)
     (cond ((> i n) temp)
-          ((= i 0) (iter (+ 1 i) (+ temp (yk 0 a h))))
-          ((= i n) (iter (+ 1 i) (+ temp (yk i a h))))
-          ((even? i) (iter (+ 1 i) (+ temp (* 2 (yk i a h)))))
-          (else (iter (+ 1 i) (+ temp (* 4 (yk i a h)))))))
+          ((= i 0) (iter (+ 1 i) (+ temp (yk 0 a))))
+          ((= i n) (iter (+ 1 i) (+ temp (yk i a))))
+          ((even? i) (iter (+ 1 i) (+ temp (* 2 (yk i a)))))
+          (else (iter (+ 1 i) (+ temp (* 4 (yk i a)))))))
   
   (* (/ h 3) (iter 0 0)))
 
