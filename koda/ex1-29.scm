9865e65854655a40bbbc2c177a18abf6657c1c66
koda-0303 <hip.hop.zip.please@gmail.com>
2014-12-05 15:01:59 +0900

    remove brank

--- koda/ex1-29.scm
+++ koda/ex1-29.scm
@@ -6,10 +6,10 @@
 (define (simpson f a b n)
   (define h (/ (- b a) n))
   (* (/ h 3) (+ (f a) (f (+ a (* n h)))(simpson-iter f a h (- n 1)))))
-(define (simpson-iter f a h n)
-  (define y (f (+ a (* n h))))
-  (define c (if (even? n) 2 4))
-  (if (> n 1) (+ (* c y) (simpson-iter f a h (- n 1)))) 0 )
+(define (simpson-iter f a h k)
+  (define y (f (+ a (* k h))))
+  (define c (if (even? k) 2 4))
+  (if (> k 1) (+ (* c y) (simpson-iter f a h (- k 1)))) 0 )
 
 (print (simpson cube 0 1 100))
 
