4a16be5761a19bcfb50e47cd20fc9d00b86ce99e
堺澤勇也 <ksf.doingmorewithless@gmail.com>
2014-12-17 13:39:57 +0900

    first commit

---
+++ sakaizawa/ex02-22.scm
@@ -0,0 +1,23 @@
+;最初のプログラム
+(define (square-list items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things) 
+              (cons (square (car things))
+                    answer))))
+  (iter items nil))
+;(1^2 . ())→(2^2 . (1^2 . ()))→(3^2 . (2^2 . (1^2 . ())))というように評価されていってしまうから、結果が逆順になってしまう
+
+;修正後
+(define (square-list items)
+  (define (iter things answer)
+    (if (null? things)
+        answer
+        (iter (cdr things)
+              (cons answer
+                    (square (car things))))))
+  (iter items nil))
+;(() . 1^2)→((() . 1^2) . 2^2)→というようにconsしていくため、うまくいかない
+
+
