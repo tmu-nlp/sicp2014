7da0b59344df481028b93fe654e485058c098593
堺澤勇也 <ksf.doingmorewithless@gmail.com>
2014-12-19 15:21:32 +0900

    modified

--- sakaizawa/ex02-03.scm
+++ sakaizawa/ex02-03.scm
@@ -8,6 +8,13 @@
 (define (start-segment seg) (car seg))
 (define (end-segment seg) (cdr seg))
 
+;周囲・面積計算
+(define (perimeter r)
+  (+ (* 2 (height-rectangle r))
+     (* 2 (width-rectangle r))))
+(define (area r)
+  (* (height-rectangle r) (width-rectangle r)))
+
 ;縦・横を表す２つの線分で表す
 (define (make-rectangle a b) (cons a b))
 (define (height-rectangle a) (length-segment (car a)))
@@ -18,8 +25,15 @@
            (square (- (y-point (start-segment a))
                       (y-point (end-segment a)))))))
 
-(define rec1 (make-rectangle 3 4))
-(print "(define rec1 (make-rectangle 3 4))")
+(define rec1 (make-rectangle (cons (cons 1 1) (cons 1 2)) (cons (cons 3 1) (cons 3 2))))
+(print "(define rec1 (make-rectangle (cons (cons 1 1) (cons 1 2)) (cons (cons 3 1) (cons 3 2))))")
+
+(print "(perimeter rec1)")
+(print (perimeter rec1))
+(print )
+(print "(area rec1)")
+(print (area rec1))
+(print )
 
 ;幅×高さの長さで表す
 (define (make-rectangle height width) (cons height width))
@@ -29,19 +43,6 @@
 (define rec2 (make-rectangle 4 6))
 (print "(define rec2 (make-rectangle 4 6))")
 
-;周囲・面積計算
-(define (perimeter r)
-  (+ (* 2 (height-rectangle r))
-     (* 2 (width-rectangle r))))
-(define (area r)
-  (* (height-rectangle r) (width-rectangle r)))
-
-(print "(perimeter rec1)")
-(print (perimeter rec1))
-(print )
-(print "(area rec1)")
-(print (area rec1))
-(print )
 (print "(perimeter rec2)")
 (print (perimeter rec2))
 (print )
