dd697c935a6a0c9a77b96df37f730701b1146afb
KeisukeNoda <code-k-noda@outlook.com>
2015-01-22 23:11:23 +0900

    first commit

---
+++ KeisukeNoda/Ex2.38-2.45/ex-2.44.rkt
@@ -0,0 +1,13 @@
+#lang planet neil/sicp
+
+(define (right-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (right-split painter (- n 1))))
+        (beside painter (below smaller smaller)))))
+
+(define (up-split painter n)
+  (if (= n 0)
+      painter
+      (let ((smaller (up-split painter (- n 1))))
+        (below painter (beside smaller smaller)))))
