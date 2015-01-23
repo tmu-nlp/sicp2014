5323691f45e321bd2889ac356cf60ba36388af5c
平田亜衣 <hirataai@hirata.local>
2015-01-23 09:03:39 +0900

    add ~2-32

---
+++ hirata/chapter_2/exercise_2_28.rkt
@@ -0,0 +1,10 @@
+(define (fringe tree)
+  (cond ((null? tree) tree)
+        ((not (pair? tree)) (list tree))
+        (else (append (fringe (car tree))
+                      (fringe (cdr tree))))))
+
+(define x (list (list 1 2) (list 3 4)))
+(display x)
+(newline)
+(display (fringe x))
