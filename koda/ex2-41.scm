36bc010679f887cedcbfe132ff44cd9fd2ec9901
koda-0303 <hip.hop.zip.please@gmail.com>
2015-02-05 00:22:16 +0900

    first commit

--- koda/ex2-41.scm
+++ koda/ex2-41.scm
@@ -3,11 +3,26 @@
 (define (square x)
   (* x x))
 
+(define (smallest-divisor n) (find-divisor n 2))
+(define (find-divisor n test-divisor)
+  (cond ((> (square test-divisor) n) n)
+		((divides? test-divisor n) test-divisor)
+		(else (find-divisor n (+ test-divisor 1)))))
+(define (divides? a b) (= (remainder b a) 0))
+
+(define (prime? n)
+  (= n (smallest-divisor n)))
+
 (define (fib n)
   (cond ((= n 0) 0)
 	((= n 1) 1)
 	(else (+ (fib (- n 1))
 		 (fib (- n 2))))))
+
+(define (enumerate-interval low high)
+  (if (> low high)
+	()
+	(cons low (enumerate-interval (+ low 1) high))))
 
 (define (accumulate op initial sequence)
   (if (null? sequence)
@@ -60,11 +75,6 @@
 (define (reverse sequence)
   (fold-left (lambda (x y) (cons y x)) () sequence))
 
-(define s (accumulate
-			append () (map (lambda (i)
-							 (map (lambda (j) (list i j))))
-								  (enumerate-interval 1 n))))
-
 (define (flatmap proc seq)
   (accumulate append () (map proc seq)))
 
@@ -83,26 +93,38 @@
 						(enumerate-interval 1 (- i 1))))
 				 (enumerate-interval 1 n)))))
 
-(define (permutation s)
-  (if (null? s)
-	(list ())
-	(flatmap (lambda (x)
-			   (map (lambda (p) (cons x p))
-					(permutations (remove x s))))
-			 s)))
-
 (define (remove item sequence)
   (filter (lambda (x) (not (= x item)))
 		  sequence))
 
-(define (unique-triples n)
-  (flatmap
-	(lambda (i)
-	  (flatmap
-		(lambda (j)
-		  (map (lambda (k) (list i j k))
-			   (enumerate-interval i (- j 1))))
-		(enumerate-interval 1 (- i 1))))
-	(enumerate-interval 1 n)))
+(define (unique-pairs n)
+  (flatmap (lambda (i)
+			 (map (lambda (j) (list i j))
+				  (enumerate-interval 1 (- i 1))))
+		   (enumerate-interval 1 n)))
+(print (unique-pairs 3))
 
-(print (unique-triples 4))
+(define (prime-sum-pairs n)
+  (map make-pair-sum
+	   (filter prime-sum?
+			   (unique-pairs n))))
+(print (prime-sum-pairs 3))
+
+(define (unique-triples n)
+  (flatmap (lambda (i)
+			 (flatmap (lambda (j)
+					(map (lambda (k) (list i j k))
+						 (enumerate-interval 1 (- j 1))))
+				  (enumerate-interval 1 (- i 1))))
+		   (enumerate-interval 1 n)))
+
+(define (same-sum-triples n s)
+  (define (same-sum? triple)
+	(= (sum triple) s))
+  (filter same-sum?
+		  (unique-triples n)))
+
+(define (sum triple)
+  (+ (car triple) (cadr triple) (caddr triple)))
+
+(print (same-sum-triples 10 10))
