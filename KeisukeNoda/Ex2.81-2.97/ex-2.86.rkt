f69dbef5386cfa7a6bbefb73886bd9a4e5c2aa24
KeisukeNoda <code-k-noda@outlook.com>
2015-02-06 17:16:22 +0900

    first commit

--- KeisukeNoda/Ex2.81-2.97/ex-2.86.rkt
+++
@@ -1,7 +0,0 @@
-#lang racket
-
-I need to modify the (install-complex-package) to use add instead of +, mul instead of *, etc. Then the generic ops will be used.
-
-The sine&cosine should be add to both rational and scheme-number packages as they are used only on that level. 
-
-
